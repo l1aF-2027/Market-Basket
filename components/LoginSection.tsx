b04c142eaeef72440288fd3bb037d574116c6dec
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-16 17:49:00 +0700

    fix the way web redirect after signin

--- components/LoginSection.tsx
+++ components/LoginSection.tsx
@@ -1,32 +1,10 @@
 "use client";
-import { SignIn, useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-// Separate component for handling redirects
-function RedirectHandler() {
-  const { user, isLoaded } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isLoaded && user) {
-      const userRole = user.publicMetadata?.role;
-      if (userRole === "admin") {
-        router.push("/admin");
-      } else {
-        router.push("/main");
-      }
-    }
-  }, [user, isLoaded, router]);
-
-  return null; // This component doesn't render anything
-}
+import { SignIn } from "@clerk/nextjs";
 
 export default function LoginSection() {
   return (
     <div className="">
-      <RedirectHandler />
-      <SignIn routing="hash" signUpUrl="/signUp" redirectUrl="/main" />
+      <SignIn routing="hash" signUpUrl="/signUp" redirectUrl="/admin" />
     </div>
   );
 }
