750ac06d780f67ca258bb58b47f15f883640f8b0
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-18 13:38:28 +0700

    change the title of admin dashboard

--- components/admin/AdminDashboard.tsx
+++ components/admin/AdminDashboard.tsx
@@ -132,19 +132,14 @@ export default function AdminDashboard() {
 
   return (
     <div className="h-screen flex flex-col sticky">
-      <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-          <h1 className="text-lg font-semibold">Admin Dashboard</h1>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">Admin Dashboard</h2>
           <div className="ml-auto flex items-center space-x-4">
             <SignedIn>
               <UserButton />
             </SignedIn>
           </div>
-        </div>
-      </div>
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         </div>
         <Tabs defaultValue="overview" className="space-y-4">
           <TabsList>
