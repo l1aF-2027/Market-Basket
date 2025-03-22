f0bba0ca302731788a61447d46ba71315dc145c2
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-22 14:53:59 +0700

    Added Google Search Console verification file

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ export const metadata = {
   icons: {
     icon: "web.ico",
   },
+  description:
+    "This is a website simualte a shopping website, using data mining techniques for suggested products.",
+  keywords:
+    "market basket, market-basket.vercel.app, market basket for data mining, data mining, data mining project, market basket project, market basket data mining, market basket data mining project, market basket data mining project website, market basket data mining project website using data mining techniques, market basket data mining project website using data mining techniques for suggested products, market basket data mining project website using data mining techniques for suggested products for shopping, market basket data mining project website using data mining techniques for suggested products for shopping website, market basket data mining project website using data mining techniques for suggested products for shopping website simualte, market basket data mining project website using data mining techniques for suggested products for shopping website simualte a shopping website",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
