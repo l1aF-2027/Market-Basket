12bd618d272ee861244edc32dc1c567eab9ba9e5
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-23 11:07:06 +0700

    add show website link when share

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,27 @@ import "./globals.css";
 
 export const metadata = {
   title: "Market Basket",
+  description:
+    "Market Basket is a demo e-commerce website that applies the Apriori algorithm for Data Mining to suggest products that customers may want to buy.",
   icons: {
     icon: "web.ico",
   },
-  description:
-    "Market Basket is a demo e-commerce website that applies the Apriori algorithm for Data Mining to suggest products that customers may want to buy.",
-  keywords:
-    "market basket, market-basket.vercel.app, market basket for data mining, data mining, data mining project, market basket project, market basket data mining, market basket data mining project, market basket data mining project website, market basket data mining project website using data mining techniques, market basket data mining project website using data mining techniques for suggested products, market basket data mining project website using data mining techniques for suggested products for shopping, market basket data mining project website using data mining techniques for suggested products for shopping website, market basket data mining project website using data mining techniques for suggested products for shopping website simualte, market basket data mining project website using data mining techniques for suggested products for shopping website simualte a shopping website",
   robots: "index, follow",
+  openGraph: {
+    title: "Market Basket - Smart Shopping Suggestions",
+    description:
+      "Discover products you may want to buy based on data mining techniques.",
+    url: "https://market-basket.vercel.app",
+    type: "website",
+    images: [
+      {
+        url: "https://storage.googleapis.com/kaggle-datasets-images/6896627/11067550/c232b880c9a1236f9c8937c3190debc9/dataset-cover.jpeg?t=2025-03-18-01-30-42",
+        width: 1200,
+        height: 630,
+        alt: "Market Basket Website Preview",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +34,31 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          <meta property="og:title" content={metadata.openGraph.title} />
+          <meta
+            property="og:description"
+            content={metadata.openGraph.description}
+          />
+          <meta property="og:url" content={metadata.openGraph.url} />
+          <meta property="og:type" content={metadata.openGraph.type} />
+          <meta
+            property="og:image"
+            content={metadata.openGraph.images[0].url}
+          />
+          <meta
+            property="og:image:width"
+            content={metadata.openGraph.images[0].width.toString()}
+          />
+          <meta
+            property="og:image:height"
+            content={metadata.openGraph.images[0].height.toString()}
+          />
+          <meta
+            property="og:image:alt"
+            content={metadata.openGraph.images[0].alt}
+          />
+        </head>
         <body>{children}</body>
       </html>
     </ClerkProvider>
