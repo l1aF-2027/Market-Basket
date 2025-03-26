e005732e1802d66c31e6edab3f2552f99ee881f9
l1aF-2027 <22520460@gm.uit.edu.vn>
2025-03-26 17:26:20 +0700

    change sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,26 +5,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://market-basket.vercel.app",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "weekly",
       priority: 1,
-    },
-    {
-      url: "https://market-basket.vercel.app/main",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
     },
     {
       url: "https://market-basket.vercel.app/signUp",
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://market-basket.vercel.app/admin",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
+      priority: 0.8,
     },
   ];
 }
